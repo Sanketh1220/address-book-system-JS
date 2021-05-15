9c756adc671c150d533635fb5476769e3cd400a0
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-05-15 18:16:16 +0530

    ADD: welcome note for address book system

---
+++ address-book-system.js
@@ -0,0 +1,9 @@
+/**
+ * Purpose: This program is written to create a address-book-system and to perform various functinalities
+ * 
+ * @author Sanketh Chigurupalli
+ * @since 14-05-2021
+ */
+
+//Printing a welcome note to console
+console.log("Welcome to address book system!");
